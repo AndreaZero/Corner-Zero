3aca183172afe8063ec86d4dd055fa3ff433e9eb
0xZeroBaby <100024338+0xZeroBaby@users.noreply.github.com>
2023-08-29 02:37:29 +0200

    push-cornerzero

--- src/components/PostPreview.jsx
+++ src/components/PostPreview.jsx
@@ -19,7 +19,7 @@ function PostPreview({ post }) {
   const truncatedText = plainText.length > 150 ? plainText.substring(0, 150) + "..." : plainText;
 
   return (
-    <Card style={{marginTop: "1rem",       width: isMobile ? '300px' : '850px',
+    <Card style={{marginTop: "1rem",       width: isMobile ? '100%' : '850px',
     background: 'rgb(0,0,0)',
 background: 'linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(56,56,56,0.7587827367275035) 100%)'
     
