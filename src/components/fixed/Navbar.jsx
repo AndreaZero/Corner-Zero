7c068d0862afa104bbc5195ec769c43e56106c76
0xZeroBaby <100024338+0xZeroBaby@users.noreply.github.com>
2023-08-29 02:30:33 +0200

    push-cornerzero

--- src/components/fixed/Navbar.jsx
+++ src/components/fixed/Navbar.jsx
@@ -41,11 +41,12 @@ function Navbar() {
                   }} src={logo} alt='logo'></img>
                   </a>
 
+                  <Hidden smDown> {/* Nasconde i seguenti elementi su schermi piccoli */}
                       <Link style={{ color: '#5CB574' }} to='/'>Home</Link>
                       <Link style={{ color: '#5CB574' }} to='/corners'>Corners</Link>
                       <Link style={{ color: '#5CB574' }} to='/repos'>Repos</Link>
                       <Link style={{ color: '#5CB574' }} to='/contacts'>Contact Me</Link>
-
+                  </Hidden>
 
                   {isAuthenticated && (
                       <Box>
