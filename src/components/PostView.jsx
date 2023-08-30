09a467f2cf5f00ec1440fa5a112b1b332cc141a0
0xZeroBaby <100024338+0xZeroBaby@users.noreply.github.com>
2023-08-30 20:30:00 +0200

    push-cornerzero

--- src/components/PostView.jsx
+++ src/components/PostView.jsx
@@ -55,7 +55,7 @@ function PostView() {
 
   const addLike = async () => {
     if (localStorage.getItem(`liked-${id}`)) {
-        setLiked('Already liked!');
+        setLiked('Liked!');
         return;
     }
 
@@ -146,7 +146,7 @@ function PostView() {
 > 
     <FontAwesomeIcon style={{marginRight: "5px"}} icon={faHeart}></FontAwesomeIcon>
     ({post.likes})
-    <h6 style={{fontSize: '12px'}}>{liked === 'Already liked!' && <span style={{ marginLeft: '10px', color: 'red' }}>{liked}</span>}</h6>
+    <h6 style={{fontSize: '12px'}}>{liked === 'Liked!' && <span style={{ marginLeft: '10px', color: 'red' }}>{liked}</span>}</h6>
     <h6 style={{fontSize: '12px'}}>{liked === 'Liked :)' && <span style={{ marginLeft: '10px', color: 'green' }}>{liked}</span>}</h6>
 </Button>
 
