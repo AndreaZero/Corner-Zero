0fb0a820a3e965f0f204945d9be1dbaab40150bb
0xZeroBaby <100024338+0xZeroBaby@users.noreply.github.com>
2023-08-31 15:45:42 +0200

    push-cornerzeroserver

--- src/components/fixed/Footer.jsx
+++ src/components/fixed/Footer.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Container, Box } from '@mui/material';
+import { Container } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaypal } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <Box
-      sx={{
-        backgroundColor: '#000',
-        color: '#fff',
-        padding: '20px 0',
-        position: 'relative',
-        bottom: 0,
-        width: '100%',
-        marginTop: "1.5rem"
-      }}
-    >
       <Container style={{display: 'flex', flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-        <h4>
-          © 2023 dev by AndreaZero.</h4>
-          <h5><a href='http://paypal.me/andreazero/' target='black' rel='noopener noreferrer'>Support me <FontAwesomeIcon icon={faPaypal}></FontAwesomeIcon> </a></h5>
+        <h5>© 2023 dev by AndreaZero.</h5>
+          <h6><a href='http://paypal.me/andreazero/' target='black' rel='noopener noreferrer'>Support me <FontAwesomeIcon icon={faPaypal}></FontAwesomeIcon> </a></h6>
       </Container>
-    </Box>
   );
 };
 
