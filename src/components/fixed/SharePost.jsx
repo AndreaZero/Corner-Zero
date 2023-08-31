51a0c4ed2cb6a7f62df94f9907f8f6d781999311
0xZeroBaby <100024338+0xZeroBaby@users.noreply.github.com>
2023-08-31 12:20:13 +0200

    push-cornerzero

--- src/components/fixed/SharePost.jsx
+++ src/components/fixed/SharePost.jsx
@@ -1,19 +1,15 @@
-import { faShare, faCopy, faLink } from '@fortawesome/free-solid-svg-icons'; // Importa l'icona di copia
+import { faShare, faLink } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react'; // Importa useState
+import React, { useState } from 'react';
 import { useMediaQuery } from '@mui/material';
 import {
-  FacebookShareButton,
   TwitterShareButton,
   WhatsappShareButton,
-  FacebookMessengerShareButton,
   RedditShareButton,
   TelegramShareButton,
-  FacebookIcon,
   TwitterIcon,
   WhatsappIcon,
   RedditIcon,
-  FacebookMessengerIcon,
   TelegramIcon
 } from 'react-share';
 
@@ -21,13 +17,13 @@ const SharePost = () => {
   const url = window.location.href; 
   const mobileWidth = 600;
   const isMobile = useMediaQuery(`(max-width: ${mobileWidth}px)`);
-  const [copied, setCopied] = useState(false); // Stato per il link copiato
+  const [copied, setCopied] = useState(false);
 
   const copyToClipboard = async () => {
     try {
-      await navigator.clipboard.writeText(url); // Copia l'URL negli appunti
-      setCopied(true); // Imposta lo stato come copiato
-      setTimeout(() => setCopied(false), 2000); // Reimposta lo stato dopo 2 secondi
+      await navigator.clipboard.writeText(url);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
     } catch (err) {
       console.error('Failed to copy url', err);
     }
@@ -52,7 +48,7 @@ const SharePost = () => {
           <FontAwesomeIcon icon={faLink} style={{ color: 'white', fontSize: '20px' }} />
         </button>
 
-        {copied && <span>Copiato!</span>} {/* Mostra un messaggio se l'URL è stato copiato */}
+        {copied && <span>Copied!</span>}
 
         <TwitterShareButton url={url} title="CornerZero.eu - Read this corner!">
           <TwitterIcon style={{ borderRadius: "0.3rem" }}  size={24} round={false} />
